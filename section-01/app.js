4433037ea3f6420aa2f28cd4e541644278e1a7cd
alikoudar <alikhoudar@gmail.com>
2024-05-01 16:43:25 +0100

    creating functions in javascript

--- section-01/app.js
+++ section-01/app.js
@@ -1,1 +1,24 @@
-alert("It's Working");
+// Exercice: functions
+
+// function isValidPassword(password, username) {
+//     if (password.length < 8 || 
+//         password.indexOf(' ') !== -1 || 
+//         password.indexOf(username) !== -1
+//     ) {
+//         return false;
+//     }
+//     return true;
+// }
+
+function isValidPassword(password, username) {
+    const tooShort = password.length < 8;
+    const hasSpace = password.indexOf(' ') !== -1; 
+    const tooSimilar = password.indexOf(username) !== -1;
+    return !tooShort && !hasSpace && !tooSimilar;
+}
+
+
+
+
+
+
