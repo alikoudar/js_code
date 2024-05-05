552e82c18ce31a705a5a391f982ad76fd7021a4e
alikoudar <alikhoudar@gmail.com>
2024-05-05 12:19:37 +0100

    More stuffs on the DOM

---
+++ section-02/app.js
@@ -0,0 +1,24 @@
+// Changing the color and background-color:
+
+// const h1 = document.querySelector('h1');
+// h1.style.color = 'pink';
+// h1.style.backgroundColor = 'yellow';
+
+// Changing multiple elements:
+// const allLis = document.querySelectorAll('li');
+// const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+
+// for (let i = 0; i < allLis.length; i++) {
+//     //console.log(allLis[i].innerText);
+//     allLis[i].innerText = "WE ARE THE CHAMPIONS" 
+// }
+
+// for (const li of allLis) {
+//     li.innerHTML = "WE ARE <b> THE CHAMPIONS </b>"
+// }
+
+// allLis.forEach((li) => li.innerHTML = "WE ARE <b> THE CHAMPIONS </b>");
+// allLis.forEach((li, i) => {
+//     const color = colors[i];
+//     li.style.color = color;
+// });
