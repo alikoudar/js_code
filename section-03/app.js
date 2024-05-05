6cb61763c6886e0fb6824c781f3c098d3e849825
alikoudar <alikhoudar@gmail.com>
2024-05-05 17:45:05 +0100

    The impossible button'

--- section-03/app.js
+++ section-03/app.js
@@ -1,9 +1,15 @@
-const btn = document.querySelector('#clicker');
+const btn = document.querySelector('button');
 
-btn.onclick = function() {
-    console.log('YOU CLICKED ME! GO AWAY!');
-}
+btn.addEventListener('mouseover', function() {
+    console.log('MOUSED OVER ME!');
+    const height = Math.floor(Math.random() * window.innerHeight);
+    const width = Math.floor(Math.random() * window.innerWidth);
+    btn.style.left = `${width}px`;
+    btn.style.top = `${height}px`;
 
-btn.ondblclick = function() {
-    console.log('DOUBLE CLICK')
-}
+});
+
+btn.addEventListener('click', function() {
+    btn.innerText = 'YOU GOT ME!';
+    document.body.style.backgroundColor = 'green';
+})
